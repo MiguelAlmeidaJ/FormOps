a2461f2744f37b031fffc495a843c99311e3930b
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-11 12:31:06 -0300

    Configura variáveis de ambiente

--- app/config/database.php
+++ app/config/database.php
@@ -1,23 +1,3 @@
 <?php
 
-// $host = 'localhost';
-// $dbname = 'forms_church';
-// $user = 'root';
-// $password = '';
-
-$host = 'localhost';
-$dbname = 'anoa0880_formops';
-$user = 'anoa0880_formops';
-$password = 'M8PAp2D7WMfggBTRZ5wP';
-
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
-        $user,
-        $password
-    );
-
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro ao conectar com o banco de dados: " . $e->getMessage());
-}
+require_once __DIR__ . '/database-env.php';
